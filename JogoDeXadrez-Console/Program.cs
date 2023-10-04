1066112ace8e4dd6b5098a95cbfd776c5a0f3bb0
Joao Corsino <oi.corsino@gmail.com>
2023-10-04 18:21:01 -0300

    Criando a classe PosicaoTabuleiroXadrez

--- JogoDeXadrez-Console/Program.cs
+++ JogoDeXadrez-Console/Program.cs
@@ -8,18 +8,10 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+        PosicaoTabuleiroXadrez posicao = new PosicaoTabuleiroXadrez('c', 7);
 
-        try
-        {
-            tabuleiro.ColocarPecaNoTabuleiro(new PecaTorre(Cor.Preta, tabuleiro), new Posicao(0, 0));
-            tabuleiro.ColocarPecaNoTabuleiro(new PecaTorre(Cor.Preta, tabuleiro), new Posicao(1, 3));
-            tabuleiro.ColocarPecaNoTabuleiro(new PecaRei(Cor.Preta, tabuleiro), new Posicao(0, 2));
-            Tela.ImprimirTabuleiro(tabuleiro);
-        }
-        catch (TabuleiroException mensagemErro)
-        {
-            Console.WriteLine(mensagemErro.Message);
-        }
+        Console.WriteLine(posicao);
+
+        Console.WriteLine(posicao.ConvertePosicao());
     }
 }
---
+++ JogoDeXadrez-Console/Xadrez/PosicaoTabuleiroXadrez.cs
@@ -0,0 +1,25 @@
+﻿using TabuleiroSpace;
+
+namespace XadrezSpace
+{
+    internal class PosicaoTabuleiroXadrez
+    {
+        public char PosicaoTabuleiroColuna { get; set; }
+        public int PosicaoTabuleiroLinha { get; set; }
+
+        public PosicaoTabuleiroXadrez(char posicaoTabuleiroColuna, int posicaoTabuleiroLinha)
+        {
+            PosicaoTabuleiroColuna = posicaoTabuleiroColuna;
+            PosicaoTabuleiroLinha = posicaoTabuleiroLinha;
+        }
+
+        public Posicao ConvertePosicao()
+        {
+            return new Posicao(8 - PosicaoTabuleiroLinha, PosicaoTabuleiroColuna - 'a');
+        }
+        public override string ToString()
+        {
+            return ($"{PosicaoTabuleiroColuna}, {PosicaoTabuleiroLinha}");
+        }
+    }
+}
